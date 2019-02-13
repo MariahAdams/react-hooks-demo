4028727128360b5b8a419c8843cdc9e23705d6b0
MariahAdams <mariah.adams23@yahoo.com>
2019-02-13 12:26:47 -0800

    old school class component

--- src/counter/Counter.js
+++ src/counter/Counter.js
@@ -1,12 +1,22 @@
 import React, { Component } from 'react';
 
 class Counter extends Component {
+  state = {
+    count: 0
+  };
+
+  setCount = () => {
+    this.setState({ count: this.state.count + 1 });
+  };
 
 
   render() {
     return (
       <div>
-        <h2>Counter Component</h2>
+        {/* this is a comment in React */}
+        <h2>{this.state.count}</h2>
+
+        <button onClick={this.setCount}>Count up to the moon</button>
       </div>
     )
   }
