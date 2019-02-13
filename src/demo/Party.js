8f963b707daf919dffbbac0a16e2a192bbf8bed5
MariahAdams <mariah.adams23@yahoo.com>
2019-02-13 12:59:42 -0800

    notes

--- src/demo/Party.js
+++ src/demo/Party.js
@@ -1,10 +1,12 @@
 import React, { useEffect } from 'react';
 
 function Party() {
+  //useEffect can take a second arg, []
+  //It will look through the array and only run the effect if one of those values has changed
   useEffect(() => {
     console.log('I have arrived at the hooks party!');
     document.title = 'present';
-  });
+  }, []);
 
   return <div>something about a party</div>;
 }
